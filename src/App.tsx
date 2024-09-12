0526105bc461c4f7e9b019653c0af4a10c83a59b
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-12 19:22:11 +0530

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home>
         <Jumbotron />
+        <Skills />
       </Home>
     </div>
   );
