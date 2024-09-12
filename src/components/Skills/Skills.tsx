82cdf24613a020063be9880fb585476e95f50bb3
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-12 22:37:41 +0530

    Added Skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,35 +3,103 @@ import "./Skills.css";
 
 const Skills: React.FC = () => {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="table-div">
+      <h1 id="table-header">Skillset</h1>
+      <table className="table">
+        <tbody>
+          <tr>
+            <td>Front-End Development:</td>
+            <td>
+              <div className="skills-div">
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\html5.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">HTML5</p>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\css3.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">CSS3</p>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\javascript.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">JavaScript</p>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\react.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">React</p>
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Back-End Development:</td>
+            <td>
+              <div className="skills-div">
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\django.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">Django</p>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\firebase.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">Firebase</p>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <img
+                    src="src\components\Skills\img\mysql.png"
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">MySQL</p>
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Programming Languages:</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <td>Version Control:</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
