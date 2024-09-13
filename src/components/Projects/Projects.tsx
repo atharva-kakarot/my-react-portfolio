6893f2eff6eda95ab1239adafd856c6d6ddccfd0
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-13 13:43:05 +0530

    Added project cards

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,56 @@
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "./Projects.css";
 
+interface Props {
+  title: string;
+  img: string;
+  text: string;
+  link: string;
+}
+
+interface Props {}
+
+const Card = ({ title, img, text, link }: Props) => {
+  return (
+    <div className="project-card">
+      <img
+        src={`src/components/Projects/img/${img}.jpeg`}
+        className="card-img"
+      />
+      <div className="project-card-body">
+        <h5 className="project-card-title">{title}</h5>
+        <p className="project-card-text">{text}</p>
+        <a href={`${link}`} className="project-links" target={"_blank"}>
+          <span className="project-button">
+            <i
+              className="fa-brands fa-github"
+              style={{ fontSize: "1.5em" }}
+            ></i>
+            GitHub
+          </span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div id="project-cards-div">
+      <Card
+        img="animemiru"
+        title="AnimeMiru"
+        text="A simple anime information library app made using pure HTML, CSS, JavaScript and AniList API."
+        link="https://github.com/atharva-kakarot/AnimeMiru"
+      />
+      <Card
+        img="weatherapp"
+        title="Simple Weather App"
+        text="A simple weather app made using HTML, CSS, Django and OpenWeather API."
+        link="https://github.com/atharva-kakarot/simple-weather-app"
+      />
+    </div>
+  );
 };
 
 export default Projects;
