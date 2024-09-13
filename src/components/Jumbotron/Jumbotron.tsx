c60e3708169c427deda14a9ee16290762fa64d53
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-13 09:56:31 +0530

    Jumbotron changes

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -8,7 +8,7 @@ const Jumbotron: React.FC = () => {
       <div id="text-div">
         <h1 className="display-4">Hi! I'm Atharva.</h1>
         <p className="lead">
-          I am a passionate Web Developer looking for opportunities to showcase
+          I am an aspiring Web Developer looking for opportunities to showcase
           and further develop my skills!
         </p>
         <p className="lead">
@@ -17,13 +17,6 @@ const Jumbotron: React.FC = () => {
             id="know-more-btn"
             href="#"
             role="button"
-            // style={{
-            //   backgroundColor: "#00FF00",
-            //   border: "none",
-            //   fontSize: "0.7em",
-            //   color: "black",
-            //   fontWeight: "500",
-            // }}
           >
             Know more
           </a>
