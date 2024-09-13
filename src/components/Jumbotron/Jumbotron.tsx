f7465f085063a72fd72305f2b37b541663a4bac3
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-13 15:36:53 +0530

    Added profile pic

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -5,23 +5,29 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Jumbotron: React.FC = () => {
   return (
     <div className="jumbotron">
-      <div id="text-div">
-        <h1 className="display-4">Hi! I'm Atharva.</h1>
-        <p className="lead">
-          I am an aspiring Web Developer looking for opportunities to showcase
-          and further develop my skills!
-        </p>
-        <p className="lead">
-          <a
-            className="btn btn-primary btn"
-            id="know-more-btn"
-            href={"https://www.linkedin.com/in/Atharva-Karanjekar"}
-            target={"_blank"}
-            role="button"
-          >
-            Know more
-          </a>
-        </p>
+      <div id="outermost-text-div">
+        <div id="text-div">
+          <h1 className="display-4">Hi! I'm Atharva.</h1>
+          <p className="lead">
+            I am an aspiring Web Developer looking for opportunities to showcase
+            and further develop my skills!
+          </p>
+          <p className="lead">
+            <a
+              className="btn btn-primary btn"
+              id="know-more-btn"
+              href={"https://www.linkedin.com/in/Atharva-Karanjekar"}
+              target={"_blank"}
+              role="button"
+            >
+              Know more
+            </a>
+          </p>
+        </div>
+        <img
+          src={"src/components/Jumbotron/img/profilepic.png"}
+          id="profile-img"
+        />
       </div>
     </div>
   );
