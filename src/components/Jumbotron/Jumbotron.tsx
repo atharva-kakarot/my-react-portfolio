4de608c244d798d982346e747887343ab3e2d551
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-14 10:47:47 +0530

    Added media queries

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -4,42 +4,44 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Jumbotron: React.FC = () => {
   return (
-    <div className="jumbotron">
-      <div id="outermost-text-div">
-        <div id="text-div">
-          <h1 className="display-4">
-            Hi! I'm{" "}
-            <small
-              style={{
-                display: "inline",
-                color: "lime",
-                fontSize: "1.04em",
-              }}
-            >
-              Atharva
-            </small>
-            .
-          </h1>
-          <p className="lead">
-            I am an aspiring Web Developer looking for opportunities to showcase
-            and further develop my skills!
-          </p>
-          <p className="lead">
-            <a
-              className="btn btn-primary btn"
-              id="know-more-btn"
-              href={"https://www.linkedin.com/in/Atharva-Karanjekar"}
-              target={"_blank"}
-              role="button"
-            >
-              Know more
-            </a>
-          </p>
+    <div id="jumbotron-div">
+      <div className="jumbotron">
+        <div id="outermost-text-div">
+          <div id="text-div">
+            <h1 className="display-4">
+              Hi! I'm{" "}
+              <small
+                style={{
+                  display: "inline",
+                  color: "lime",
+                  fontSize: "1.04em",
+                }}
+              >
+                Atharva
+              </small>
+              .
+            </h1>
+            <p className="lead">
+              I am an aspiring Web Developer looking for opportunities to
+              showcase and further develop my skills!
+            </p>
+            <p className="lead">
+              <a
+                className="btn btn-primary btn"
+                id="know-more-btn"
+                href={"https://www.linkedin.com/in/Atharva-Karanjekar"}
+                target={"_blank"}
+                role="button"
+              >
+                Know more
+              </a>
+            </p>
+          </div>
+          <img
+            src={"src/components/Jumbotron/img/profilepic.png"}
+            id="profile-img"
+          />
         </div>
-        <img
-          src={"src/components/Jumbotron/img/profilepic.png"}
-          id="profile-img"
-        />
       </div>
     </div>
   );
