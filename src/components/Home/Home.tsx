5dbf953817e063e02c0dcc80f7bd34b1d99b41e2
atharva-kakarot <atharva.kakarot@gmail.com>
2024-09-14 09:35:56 +0530

    Added media queries

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import type { Container, Engine } from "tsparticles-engine";
 import Particles from "react-tsparticles";
 //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import { loadSlim } from "tsparticles-slim";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
